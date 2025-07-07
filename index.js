1174e076c8a3cf9a75e1839b6410a5ecad8304c1
Yug Sutariya <yugsutariya@yugs-air.home>
2025-07-07 16:03:25 -0400

    changes in code

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("Hello from SonarQube test!");
+console.log("Debugging mode enabled!");
+
+var unusedVar = 42;
+
+function tooManyParams(a, b, c, d, e, f) {
+    if (a > 0) {
+        if (b > 0) {
+            if (c > 0) {
+                console.log("Nested too deep");
+            }
+        }
+    }
+}
+
+function duplicate1() {
+    let x = 0;
+    x++;
+    x++;
+    x++;
+}
+
+function duplicate2() {
+    let x = 0;
+    x++;
+    x++;
+    x++;
+}
+
+try {
+    riskyOperation();
+} catch (e) {
+}
+
+function riskyOperation() {
+    return 1 / 0;
+}
+
+tooManyParams(1, 2, 3, 4, 5, 6);
